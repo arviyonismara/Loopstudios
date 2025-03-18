fda34d79533991a9b8f95d51cd066eb8093e0247
Arvie Yonismara <arviyonismara@gmail.com>
2025-03-18 21:27:02 +0700

    FInishing 1

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,41 @@ function App() {
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
               </div>
             </div>
-            {/* @todo Hamburger Button */}
+            {/* Hamburger Button */}
+            <div className="md:hidden">
+              <button
+                id="menu-btn"
+                type="button"
+                className="open z-40 block hamburger md:hidden focus:outline-none"
+              >
+                <span className="hamburger-top"></span>
+                <span className="hamburger-middle"></span>
+                <span className="hamburger-bottom"></span>
+              </button>
+            </div>
           </nav>
 
           {/* @todo Mobile Menu */}
+          <div
+            id="menu"
+            className="absolute top-0 bottom-0 left-0 hidden flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"
+          >
+            <a href="" className="hover:text-pink-500">
+              About
+            </a>
+            <a href="" className="hover:text-pink-500">
+              Careers
+            </a>
+            <a href="" className="hover:text-pink-500">
+              Events
+            </a>
+            <a href="" className="hover:text-pink-500">
+              Products
+            </a>
+            <a href="" className="hover:text-pink-500">
+              Supports
+            </a>
+          </div>
 
           <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl">
             Impressive Experiences that deliver
@@ -284,6 +315,7 @@ function App() {
                 </div>
               </div>
             </div>
+
             {/* Social & Copy Container */}
             <div className="flex flex-col items-start justify-between space-y-4 text-gray-500">
               {/* Icons Container */}
